ff10d930588b487e086826cc6c62ecd16e6609d9
eddie cianci <defeated@users.noreply.github.com>
2018-06-18 17:16:49 -0400

    bump version used in brew formula
    fixes macOS error -
    
      failed MSpanList_Insert 0x118000 0x1a987f9fbdba3 0x0
      fatal error: MSpanList_Insert

--- touchp.rb
+++ touchp.rb
@@ -1,5 +1,5 @@
 class Touchp < Formula
-  url       "https://github.com/defeated/touchp/releases/download/0.0.1/touchp-0.1.1-darwin-amd64.zip"
+  url       "https://github.com/defeated/touchp/releases/download/0.0.2/touchp-0.0.2-darwin-amd64.zip"
   homepage  "https://github.com/defeated/touchp/"
   desc      "basically `mkdir -p` + `touch`, written in Go"
   sha256    "f0275b34d87c624541dd04824815290666bc6de5055632d32d4d154507e27ed0"
